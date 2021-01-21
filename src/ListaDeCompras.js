b853352a0e091c915906c03a91de2a041fd9f9d5
Vitor <vitor.cutri@hotmail.com>
2021-01-20 23:23:35 -0300

    Resolução do exercício

---
+++ src/ListaDeCompras.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Item from "./item";
+
+function ListaDeCompras( ) {
+    return(
+        <>
+        <Item nome="Batata"/>
+        <Item nome="feijão"/>
+        <Item nome="arroz"/>
+        <Item nome="queijo"/>
+        <Item nome="ovos"/>
+        </>
+    )
+}
+
+export default ListaDeCompras;
